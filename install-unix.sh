a9baa4e5f8181914e047ab68ebda87982430651f
Realnotema <tematsyganckov@yandex.ru>
2024-05-22 12:24:40 +0000

    added install docker & docker-compose

--- install-unix.sh
+++ install-unix.sh
@@ -2,15 +2,15 @@
 
 if [[ "$(uname)" == "Linux" ]]; then
     if command -v apt-get &> /dev/null; then
-        sudo apt-get install -y nmap masscan zmap
+        sudo apt-get install -y nmap masscan zmap docker docker-compose
     elif command -v yum &> /dev/null; then
-        sudo yum install -y nmap masscan zmap
+        sudo yum install -y nmap masscan zmap docker docker-compose
     else
         echo "Не удалось определить пакетный менеджер"
         exit 1
     fi
 elif [[ "$(uname)" == "Darwin" ]]; then
-    brew install nmap masscan zmap
+    brew install nmap masscan zmap docker docker-compose
 else
     echo "Не поддерживаемая операционная система"
     exit 1
